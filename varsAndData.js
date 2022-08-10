e42ab1e76d7180aacc44dc79be9a29ed9f461069
Samitakarke <samitakarke@gmail.com>
2022-08-10 13:36:53 -0400

    First upload

---
+++ varsAndData.js
@@ -0,0 +1,115 @@
+// How do we assign a value to a variable?
+// Answer -Using the assignment operator
+// let q=5;
+// let a=10;
+
+
+
+// How do we change the value of a variable?
+// let x=5;
+// console.log(x);
+// x=3;
+// console.log(x);
+
+
+
+// How do we assign an existing variable to a new variable?
+// We should new vairiable with an assignment operator and then list your existing variable.
+// for example
+// let myName= "Samita";
+// let yourName= myName;
+
+
+
+
+// Remind me, what are declare, assign, and define?
+ 
+
+
+// What is pseudocoding and why should you do it?
+// Pseudocoding is an informal way of writing programs in which we represent the sequence of actions and instructions in form that humans can easily understand.
+
+// We should do the Pseudocoding because it allows us to plan instructions that follows a logical pattern, without including technical details.
+
+
+// // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// About 80% of time should be spent thinking about solving problems and the rest 50% to actually code to solve it.
+
+
+// Strings
+
+// Create a variable called firstVariable
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// What is the value of firstVariable?
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+
+
+//  let firstVariable = "Hello World";
+
+// firstVariable= 5;
+
+//  let secondVariable = firstVariable;
+
+//  secondVariable ="Hi friends";
+
+//  console.log(firstVariable);
+
+// let yourName= "Samita Karki";
+//  console.log("Hello, my name is " + yourName);
+
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' == 'Name');
+// // FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false == false == false == false == false!= true);
+// console.log(false ==false)
+// console.log(e== 'Kevin');
+// console.log(a < b < c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a === a && a< d); // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 !== '48');
+
+
+// D. The farm
+
+//     Declare a variable animal. Set it to be either "cow" or something else
+//     Write code that will print out "mooooo" if the it is equal to cow
+//     Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow."
+//     Commit
+
+// let animal = "cow";
+// if(animal==="cow")
+// {
+//     console.log("moooo");
+// }
+
+// {    
+//      console.log("Hey! You're not a cow.")
+// }
+
+
+// E. Driver's Ed
+
+//     Make a variable that holds a person's age; be semantic
+//     Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+
+// let age;
+
+// if (age>=16)
+// {
+//     console.log("Here are the keys.");
+// }
+
+// else if (age < 16)
+// {
+//     console.log("Sorry, you're too young.");
+// }
